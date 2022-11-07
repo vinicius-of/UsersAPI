74e8f04c8e10f225ec7a929b0778c9d9e43c552e
Vinicius <vinicius.oliveirafonseca@gmail.com>
2022-11-07 01:08:29 -0300

    setup: created the users list into a file

---
+++ data.ts
@@ -0,0 +1,22 @@
+import User from "./interfaces/models/User";
+
+export const UsersData: User[] = [
+    {
+      id: 132165,
+      nome: "Lucas",
+      empresa: "Keller Williams",
+      permissao: "ADMIN",
+    },
+    {
+      id: 14564,
+      nome: "Aline",
+      empresa: "Keller Williams",
+      permissao: "USER",
+    },
+    {
+      id: 22314,
+      nome: "Bruno",
+      empresa: "Keller Williams",
+      permissao: "USER",
+    },
+];

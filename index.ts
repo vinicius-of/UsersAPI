9cea96e208afbf613c36d125b71af82fb8075586
Vinicius <vinicius.oliveirafonseca@gmail.com>
2022-11-07 01:08:53 -0300

    task: removing the users from index.ts

--- index.ts
+++ index.ts
@@ -5,27 +5,6 @@ import ENV from './setup-env';
 import SetupEnv from './interfaces/configurations/setup-env';
 import setMiddlewares from './middlewares';
 
-let users = [
-  {
-    id: 132165,
-    nome: "Lucas",
-    empresa: "Keller Williams",
-    permissao: "ADMIN",
-  },
-  {
-    id: 14564,
-    nome: "Aline",
-    empresa: "Keller Williams",
-    permissao: "USER",
-  },
-  {
-    id: 22314,
-    nome: "Bruno",
-    empresa: "Keller Williams",
-    permissao: "USER",
-  },
-];
-
 dotenv.config();
 const app = createExpressApp();
 openServer(app, ENV);

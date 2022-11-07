a767ec7c9e82f5cf217ec147cb85a7102db49689
Vinicius <vinicius.oliveirafonseca@gmail.com>
2022-11-07 13:09:38 -0300

    setup: adding new user admin to the list

--- data.ts
+++ data.ts
@@ -1,22 +1,28 @@
 import User from "./interfaces/models/User";
 
 export const UsersData: User[] = [
-    {
-      id: 132165,
-      nome: "Lucas",
+  {
+    id: 132165,
+    nome: "Lucas",
+    empresa: "Keller Williams",
+    permissao: "ADMIN",
+  },
+  {
+    id: 14564,
+    nome: "Aline",
+    empresa: "Keller Williams",
+    permissao: "USER",
+  },
+  {
+    id: 22314,
+    nome: "Bruno",
+    empresa: "Keller Williams",
+    permissao: "USER",
+  },
+  {
+      id: 1,
+      nome: "admin",
       empresa: "Keller Williams",
-      permissao: "ADMIN",
-    },
-    {
-      id: 14564,
-      nome: "Aline",
-      empresa: "Keller Williams",
-      permissao: "USER",
-    },
-    {
-      id: 22314,
-      nome: "Bruno",
-      empresa: "Keller Williams",
-      permissao: "USER",
-    },
+      permissao: "ADMIN",  
+  },
 ];
